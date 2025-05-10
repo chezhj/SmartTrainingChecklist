f2d1d1ab4df57fb6a170d13bc1fb99d2b8e26f53
Hendrik-Jan <h@vdwaal.net>
2025-05-10 09:23:52 +0200

    fix(settings): Fix bug in static root path

--- smart_training_checklist/settings/prod.py
+++ smart_training_checklist/settings/prod.py
@@ -1,8 +1,10 @@
 """Production settings file"""
 
 # pylint: disable=unused-wildcard-import,wildcard-import
+import os
 from decouple import config
 from .base import *
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
@@ -15,11 +17,11 @@ ALLOWED_HOSTS = [
     "fly.vdwaal.net",
 ]
 
-WWW_DIR = Path(BASE_DIR).resolve().parent / "public_html/checklist/"
+WWW_DIR = os.path.join(Path(BASE_DIR).resolve(), "public_html")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = WWW_DIR / "static/"
+STATIC_ROOT = os.path.join(WWW_DIR, "static")
 STATIC_URL = "static/"
 
 SIMBRIEF_URL = "https://www.simbrief.com/api/xml.fetcher.php?userid="
